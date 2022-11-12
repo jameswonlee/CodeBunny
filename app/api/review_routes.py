45e6e5e05c7107ffeac5b06dc57078fc89a5c1d2
Keerthana Yellapragada <keerthanay@gmail.com>
2022-11-12 13:22:15 -0800

    create form

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -1,11 +1,11 @@
 from ..models import db, Coder, User, Review
 from flask import Blueprint, render_template, url_for, redirect, request, jsonify
-
-
+from flask_login import current_user, login_user, logout_user, login_required
+from ..forms.create_review import CreateReviewForm
 
 #REVIEWS
 # Create a Review for a Coder					POST- “/reviews”
-# Eager Load: Users, Coders	
+# Eager Load: Users, Coders
 # Error Validations: User cannot leave more than 1 review per Coder
 # Status code: 201 created ok, 400 error
 # Require Auth:  Logged In , userId == project.coder_id and Project.completed = true
@@ -22,6 +22,7 @@ from flask import Blueprint, render_template, url_for, redirect, request, jsonif
 # Key into instance method (route)
 review_bp = Blueprint("review_routes", __name__, url_prefix='/api/reviews')
 
+#  route to get all reviews
 @review_bp.route("/")
 def get_all_reviews():
     all_reviews = Review.query.all()
@@ -33,9 +34,39 @@ def get_all_reviews():
         return response, 200
     return "404 NOT FOUND", 404
 
+
+# route to create a new review
 @review_bp.route("/new", methods=["POST"])
+@login_required
 def create_new_review():
-    
-    return 
+    # create a new instance of reviewform
+    new_review_form = CreateReviewForm()
 
+    # if the form passes all validations and is submitted succesfully...
+    if new_review_form.validate_on_submit:
 
+        #  key into form's response data(user's input) and save all the data to a variable
+        review_data = new_review_form.data
+        print(new_review_form.data)
+
+        # Create a NEW INSTANCE of a review and populate it with the user's saved review data
+        new_review = Review()
+        new_review.populate_obj(review_data)
+
+        # another way to do this:
+        # new_review = Review(rating= review_data["rating"],review=review_data["review"])
+
+        # add the newly created review and save it to the database
+        db.session.add(new_review)
+        db.session.commit()
+
+        new_review_obj = new_review.to_dict()
+        response = {
+           new_review_obj
+        }
+
+        # return new_review
+        # redirect to the homepage
+        return new_review_obj, 201
+
+    return "Error with submission", 302
--- app/forms/create_review.py
+++ app/forms/create_review.py
@@ -5,6 +5,9 @@ from app.models import User, Review
 from flask_login import current_user, login_user, logout_user, login_required
 
 
-class ReviewForm(FlaskForm):
-    review = StringField("Your Review", validators=[DataRequired()])
+ratings = [1,2,3,4,5,6,7,8,9,10]
 
+class CreateReviewForm(FlaskForm):
+    review = StringField("Your Review", validators=[DataRequired()])
+    rating = SelectField("Choose a rating", validators=[DataRequired()], choices=ratings)
+    submit = SubmitField('Submit Your Review')
