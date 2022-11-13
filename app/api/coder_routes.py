933d05eadad7e7d67c0752ce3d445ba8fa0eff2b
Keerthana Yellapragada <keerthanay@gmail.com>
2022-11-12 21:48:49 -0800

    delete reviews route create reviews and eager loading

--- app/api/coder_routes.py
+++ app/api/coder_routes.py
@@ -2,12 +2,13 @@ from flask import Blueprint, render_template, url_for, redirect, request, jsonif
 from ..models import Coder, Project, Skill, User, Review, db
 from ..forms.create_coder import CreateCoderForm
 from flask_login import current_user, login_user, logout_user, login_required
-
+from ..forms.create_review import CreateReviewForm
 coder_bp = Blueprint("coder_routes", __name__, url_prefix="/api/coders")
 
 # Get all coders
 @coder_bp.route("/", methods=["GET"])
 def get_all_coder():
+    # ADD EAGER LOADING OF FIRSTNAME LAST NAME FROM USERS TABLES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     all_coders = Coder.query.all()
     response = {}
     if all_coders:
@@ -24,6 +25,7 @@ def get_all_coder():
 @coder_bp.route("/<int:coder_id>", methods=["GET"])
 def get_coder_profile(coder_id):
     # print('coderId', coder_id)
+    # ADD EAGER LOADING OF FIRSTNAME LAST NAME FROM USERS TABLES!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     coder = Coder.query.filter(Coder.id == coder_id).first()
 
 
@@ -71,6 +73,7 @@ def create_coder():
         print("did this run 5")
 
         new_coder_obj = new_coder.to_dict()
+        # ADD EAGER LOADING OF FIRSTNAME LAST NAME FROM USERS TABLES/logged in user session and ADD TO RESPONSE OBJECT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         return new_coder_obj, 201
 
     return {"error": "validation error"}, 401
@@ -79,3 +82,53 @@ def create_coder():
 
 
 
+
+# route to create a new review
+@coder_bp.route("/<int:coder_id>/reviews/new", methods=["POST"])
+# @login_required
+def create_new_review(coder_id):
+
+    # create a new instance of reviewform
+    new_review_form = CreateReviewForm()
+    new_review_form['csrf_token'].data = request.cookies['csrf_token']
+    # if the form passes all validations and is submitted succesfully...
+    if new_review_form.validate_on_submit():
+
+        # key into form's response data(user's input) and save all the data to a variable
+        review_data = new_review_form.data
+        print(new_review_form.data)
+
+        # Create a NEW INSTANCE of a review and populate it with the user's saved review data
+        new_review = Review()
+        new_review_form.populate_obj(new_review)
+
+
+        # find the coder from db that matches the id from params
+        current_coder = Coder.query.filter(Coder.id == coder_id).first()
+        print("current coder",current_coder)
+
+
+        new_review = Review(rating= review_data["rating"],review=review_data["review"], user_id=1, coder_id=current_coder.id)
+
+        # add the newly created review and save it to the database
+        db.session.add(new_review)
+        db.session.commit()
+
+    # ADD EAGER LOADING OF FIRSTNAME LAST NAME FROM USERS TABLES and add to response obj!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        new_review_obj = new_review.to_dict()
+        return new_review_obj, 201
+
+    return "Error with submission", 302
+
+
+# @review_bp.route("/reviews/<int:review_id>", methods=["DELETE"])
+# def delete_review(review_id):
+
+#     current_review = Review.query.get(Review.id==review_id)
+
+#     if current_review:
+#         db.session.delete(current_review)
+#         db.session.commit()
+
+#         return "succesfully deleted"
+#     return "404 review not found"
--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -2,7 +2,7 @@ from ..models import db, Coder, User, Review
 from flask import Blueprint, render_template, url_for, redirect, request, jsonify
 from flask_login import current_user, login_user, logout_user, login_required
 from ..forms.create_review import CreateReviewForm
-
+from sqlalchemy.orm import joinedload
 #REVIEWS
 # Create a Review for a Coder					POST- “/reviews”
 # Eager Load: Users, Coders
@@ -22,65 +22,40 @@ from ..forms.create_review import CreateReviewForm
 # Key into instance method (route)
 review_bp = Blueprint("review_routes", __name__, url_prefix='/api/reviews')
 
+
+
+@review_bp.route("/<int:review_id>/", methods=["DELETE"])
+def delete_review(review_id):
+
+    current_review = Review.query.get(review_id)
+
+    if current_review:
+        print("IS IT HITTING THIS DELETE")
+        db.session.delete(current_review)
+        db.session.commit()
+
+        return "succesfully deleted"
+    return "404 review not found"
+
+
 #  route to get all reviews
 @review_bp.route("/")
 def get_all_reviews():
     all_reviews = Review.query.all()
+
     response = {}
     if all_reviews:
         for review in all_reviews:
+            review_user = User.query.filter(User.id == review.user_id).first()
+            print(review_user)
             review_obj = review.to_dict()
+            review_obj["User"] = review_user.to_dict()
             response[review_obj["id"]] = review_obj
         return response, 200
     return "404 NOT FOUND", 404
 
+# open_orders = Order.query.filter(
+#         Order.finished == False and Order.employee_id == current_user.id
+#     ).options(joinedload(Order.table)).options(joinedload(Order.details, OrderDetail.menu_item))
 
-# route to create a new review
-@review_bp.route("/new", methods=["POST"])
-# @login_required
-def create_new_review():
-    # create a new instance of reviewform
-    new_review_form = CreateReviewForm()
-
-    # if the form passes all validations and is submitted succesfully...
-    if new_review_form.validate_on_submit:
-
-        #  key into form's response data(user's input) and save all the data to a variable
-        review_data = new_review_form.data
-        print(new_review_form.data)
-
-        # Create a NEW INSTANCE of a review and populate it with the user's saved review data
-        # new_review = Review()
-        # new_review.populate_obj(review_data)
-
-        coders = Coder.query.all()
-        coder_id = 0
-        if coders:
-            for coder in coders:
-                coder_obj = coder.to_dict()
-                if coder_obj["name"] == review_data.coder:
-                    coder_id = coder_obj["id"]
-                continue
-            return coder_id
-    else:
-        return {"message": "bad data"}
-
-
-
-        # another way to do this:
-        new_review = Review(rating= review_data["rating"],review=review_data["review"], user_id=1, coder=coder_id)
-        
-        # add the newly created review and save it to the database
-        db.session.add(new_review)
-        db.session.commit()
-
-        new_review_obj = new_review.to_dict()
-        # response = {
-        #    new_review_obj
-        # }
-
-        # return new_review
-        # redirect to the homepage
-        return new_review_obj, 201
-
-    return "Error with submission", 302
+#     foods = MenuItem.query.join(MenuItemType).order_by(MenuItemType.name, MenuItem.name).options(joinedload(MenuItem.type))
