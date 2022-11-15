18a0f739390997fbd8c3537ae84903513b81ac6b
James Lee <jamesleex86@gmail.com>
2022-11-15 14:04:33 -0800

    Finished reviews component

--- react-app/src/components/Reviews/index.js
+++ react-app/src/components/Reviews/index.js
@@ -2,55 +2,49 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, NavLink, Route, useParams } from 'react-router-dom';
 import { loadAllReviews } from '../../store/reviews';
-// import { loadAllSpots, loadOneSpot } from '../../store/spots'
-// import './ReviewsBrowser.css'
 
-// REVIEWS FOR EACH SPOT
+
 const Reviews = () => {
-    const dispatch = useDispatch(); // invoke dispatch
-    let { coderId } = useParams(); // use params
-    coderId = parseInt(coderId)
-    console.log("hi")
- useEffect(() => {
+    const dispatch = useDispatch();
+    let { coderId } = useParams();
+    const sessionUser = useSelector(state => state.session.user);
+    const reviewsData = useSelector(state => state.reviews);
+    const reviews = Object.values(reviewsData);
+    const boyNames = ["Demo", "Marnie", "Bobbie"];
+    
+    useEffect(() => {
         dispatch(loadAllReviews());
-        // dispatch(loadOneSpot(spotId))
-    }, [dispatch])
-
-    // let currentCoder = useSelector(state => state.oder)
-    // // currentCoder = currentCoder[coderId]
-
-    // console.log(coderId)
+    }, []);
 
 
-    // const sessionUser = useSelector(state => state.session.user)
-    // let userId;
-
-    //   if (sessionUser) {
-    //       let userId = sessionUser.id
-    //   }
-
-
-    const allReviews = useSelector(state => Object.values(state.reviews));
-    console.log("allreviews", allReviews)
-
-    // let filteredReviews = allReviews.filter(review => review.coder_Id === coderId)
-
-
-    return ( <h1> KEER SAYS HI!!!</h1>)
-
-            {/* (filteredReviews.map(review => {
-                        return (
-                                <>
-                                    <div>{review.id}</div>
-                                    <div>{review.user_id}</div>
-                                    <div>{review.bio}</div>
-                                    <div>{review.experience}</div>
-                                    <div>{review.daily_rate}</div>
-                                </>
-                            )
-             })) */}
-
-    //  )
+    return (
+        <>
+            <div className="reviews">{reviews?.map((review, index) => (
+                <div key={review.id} className="review-card">
+                    <div className="review-content">
+                        <div className="user-name">
+                            <img
+                                width={50}
+                                height={50}
+                                src={`https://randomuser.me/api/portraits/${boyNames.includes(review?.first_name) ? "women" : "men"}/${index + 1}.jpg`}
+                                className="user-image">
+                            </img>
+                            <span className="review-user-name">{review.user?.first_name}</span>
+                        </div>
+                        <div className="review">
+                            {review.review}
+                        </div>
+                        {/* {sessionUser?.id === review.User.id &&
+                                <div className="delete-review-modal">
+                                    <DeleteReviewModal review={review} />
+                                </div>
+                            } */}
+                    </div>
+                </div>
+            ))}
+            </div>
+        </>
+    )
 }
 
 
