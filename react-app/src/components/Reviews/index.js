9ea3cb2a41c87f75733d6d98bd67f1412e386da5
James Lee <jamesleex86@gmail.com>
2022-11-15 16:47:26 -0800

    Copied and pasted code

--- react-app/src/components/Reviews/index.js
+++ react-app/src/components/Reviews/index.js
@@ -11,39 +11,39 @@ const Reviews = () => {
     const reviewsData = useSelector(state => state.reviews);
     const reviews = Object.values(reviewsData);
     const boyNames = ["Demo", "Marnie", "Bobbie"];
-    
+    console.log('reviews', reviews)
+
     useEffect(() => {
         dispatch(loadAllReviews());
     }, []);
 
 
     return (
-        <>
+        <div className="reviews-container">
+            <h3>Reviews for </h3>
             <div className="reviews">{reviews?.map((review, index) => (
                 <div key={review.id} className="review-card">
-                    <div className="review-content">
-                        <div className="user-name">
-                            <img
-                                width={50}
-                                height={50}
-                                src={`https://randomuser.me/api/portraits/${boyNames.includes(review?.first_name) ? "women" : "men"}/${index + 1}.jpg`}
-                                className="user-image">
-                            </img>
-                            <span className="review-user-name">{review.user?.first_name}</span>
-                        </div>
-                        <div className="review">
-                            {review.review}
-                        </div>
-                        {/* {sessionUser?.id === review.User.id &&
+                    <div className="user-photo">
+                        <img
+                            width={50}
+                            height={50}
+                            src={`https://randomuser.me/api/portraits/${boyNames.includes(review?.first_name) ? "women" : "men"}/${index + 1}.jpg`}
+                            className="user-image">
+                        </img>
+                        <span className="review-user-name">{review.user?.first_name}</span>
+                    </div>
+                    <div className="review">
+                        {review.review}
+                    </div>
+                    {/* {sessionUser?.id === review.User.id &&
                                 <div className="delete-review-modal">
                                     <DeleteReviewModal review={review} />
                                 </div>
                             } */}
-                    </div>
                 </div>
             ))}
             </div>
-        </>
+        </div>
     )
 }
 
