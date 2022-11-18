0a5385a183167183facb0ce096df33a39bde9b8c
Jason Kong <jrkong216@gmail.com>
2022-11-17 20:48:46 -0800

    more improvements

--- react-app/src/components/SearchBar/SearchBar.js
+++ react-app/src/components/SearchBar/SearchBar.js
@@ -54,39 +54,39 @@ if (!codersArray){
     const filteredCoders = getFilteredItems(search, codersArray)
     // console.log("this is filteredCoders", filteredCoders)
     return (
-
+<>
+<div className = "whole-container">
         <div className="search-bar-container">
-        <div className="search-bar">
+            <div className="search-bar">
                 <input className="search-input"
                     type="search"
-                    placeholder="Start your search!"
+                    placeholder="Search for coders with the coding skills you need!"
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
                 />
                 <div className="icon-container">
                 <i className="icon fa fa-search fa-2x" onClick= {() => submitHandler()}></i>
                 </div>
-                <div>
-        {search ==='' ? null: (<h2 className='search-header'>Here are coders that match your search results</h2>)}
-            <div className='search-results-cards'>
+            </div>
+        </div>
 
+
+            <div className='search-results-cards'>
+            {search ==='' ? null: (<div className='search-header'>Here are coders that match your search results</div>)}
                 {filteredCoders.map(coder => {
                     return (
                     <div className="coder-shortbio-container">
-                        <div className='coder-details-title  coder-results-name' >{coder.user.first_name} {coder.user.last_name}</div>
-                        <div className='coder-details-title'>Bio:</div>
-                         <div className = 'coder-details' >{coder.bio}</div>
-                        <div className='coder-details-title'>Experience: </div>
-                         <div className = 'coder-details'>{coder.experience}</div>
-                        <button className="search-results-button"><NavLink className = 'search-results-button-link' to={`/coders/${coder.id}`}>View Profile</NavLink></button>
-
+                        <div>{coder.user.first_name} {coder.user.last_name}</div>
+                        <div>Bio: {coder.bio}</div>
+                        <div>Experience: {coder.experience}</div>
+                        <button className="button-profile"><NavLink to={`/coders/${coder.id}`}>View Profile</NavLink></button>
                     </div>
                     )
                 })}
-
-        </div>
-        </div>
-        </div>
-        </div>
+            </div>
+</div>
+        </>
     )
 }
+
+        // {search ==='' ? null: (<h2 className='search-header'>Here are coders that match your search results</h2>)}
